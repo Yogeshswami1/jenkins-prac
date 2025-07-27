37a478d6ef646144cf096324ea4bb37f84a2b569
Yogesh <yogeshkumar86077@gmail.com>
2025-07-27 15:19:58 +0530

    rr

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Layout>
       <Header style={{ backgroundColor: '#001529', padding: '0 20px' }}>
         <Title style={{ color: 'white', margin: 0 }} level={3}>
-          School Record Tracker Of Swami ji hanji bhai.........
+          School Record Tracker Of Swami ji hanji bhai..........
         </Title>
       </Header>
 
