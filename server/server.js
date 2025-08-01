3f0c44ffedc0e61f79110ba47dcc8b1a81c59e50
Yogesh <yogeshkumar86077@gmail.com>
2025-08-01 14:46:55 +0530

    assa

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express()
 // }));app.use(express.json());app.use(express.json())
 
 app.use(cors({
-  origin: ['http://books.yogeshtech.xyz', 'http://school.yogeshtech.xyz'],
+  origin: ['http://books.yogeshtech.xyz', 'http://localhost:5173','http://school.yogeshtech.xyz'],
   credentials: true
 }));
 
